7afaa8f1a6285c15d3264f11b232bbbd4ce15127
Nick <nicholas_petrocelli@brown.edu>
2022-12-02 01:29:32 -0500

    trying with 1?

--- run_scripts/run_tiered_mini_tests.sh
+++ run_scripts/run_tiered_mini_tests.sh
@@ -11,6 +11,6 @@
 # notify
 # -m abes
 #
-qsub -l gpus=1 -cwd -e test_errors/mini_5shot_cos_nocorr_err.txt -o test_results/mini_5shot_cos_nocorr_out.psv -N mini_5shot_cos_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_5shot_cos.yaml 5
-qsub -l gpus=1 -cwd -e test_errors/mini_5shot_cos_corr_err.txt -o test_results/mini_5shot_cos_corr_out.psv -N mini_5shot_cos_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_5shot_cos.yaml 5
+# qsub -l gpus=1 -cwd -e test_errors/mini_5shot_cos_nocorr_err.txt -o test_results/mini_5shot_cos_nocorr_out.psv -N mini_5shot_cos_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_5shot_cos.yaml 5
+qsub -l gpus=1 -cwd -e test_errors/mini_5shot_cos_corr_err.txt -o test_results/mini_5shot_cos_corr_out.psv -N mini_5shot_cos_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_5shot_cos.yaml 1
 
