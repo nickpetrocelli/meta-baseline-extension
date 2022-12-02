cf9ca6c612e8c2aa727f4fc21ae1dcee4400a840
Nick <nicholas_petrocelli@brown.edu>
2022-12-02 02:04:45 -0500

    don't overload the clusters

--- run_scripts/run_tiered_mini_tests.sh
+++ run_scripts/run_tiered_mini_tests.sh
@@ -16,24 +16,38 @@
 qsub -m abes -l gpus=1 -cwd -e test_errors/mini_5shot_cos_nocorr_err.txt -o test_results/mini_5shot_cos_nocorr_out.psv -N mini_5shot_cos_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_5shot_cos.yaml 5
 qsub -m abes -l gpus=1 -cwd -e test_errors/mini_5shot_cos_corr_err.txt -o test_results/mini_5shot_cos_corr_out.psv -N mini_5shot_cos_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_5shot_cos.yaml 5
 
+sleep 1h
+
 qsub -m abes -l gpus=1 -cwd -e test_errors/mini_5shot_sqr_nocorr_err.txt -o test_results/mini_5shot_sqr_nocorr_out.psv -N mini_5shot_sqr_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_5shot_sqr.yaml 5
 qsub -m abes -l gpus=1 -cwd -e test_errors/mini_5shot_sqr_corr_err.txt -o test_results/mini_5shot_sqr_corr_out.psv -N mini_5shot_sqr_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_5shot_sqr.yaml 5
+
+sleep 1h
 
 qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/mini_20shot_cos_nocorr_err.txt -o test_results/mini_20shot_cos_nocorr_out.psv -N mini_20shot_cos_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_20shot_cos.yaml 20
 qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/mini_20shot_cos_corr_err.txt -o test_results/mini_20shot_cos_corr_out.psv -N mini_20shot_cos_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_20shot_cos.yaml 20
 
+sleep 1h
+
 qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/mini_20shot_sqr_nocorr_err.txt -o test_results/mini_20shot_sqr_nocorr_out.psv -N mini_20shot_sqr_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_20shot_sqr.yaml 20
 qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/mini_20shot_sqr_corr_err.txt -o test_results/mini_20shot_sqr_corr_out.psv -N mini_20shot_sqr_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_20shot_sqr.yaml 20
+
+sleep 1h
 
 # tiered
 qsub -m abes -l gpus=1 -cwd -e test_errors/tiered_5shot_cos_nocorr_err.txt -o test_results/tiered_5shot_cos_nocorr_out.psv -N tiered_5shot_cos_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_tiered_5shot_cos.yaml 5
 qsub -m abes -l gpus=1 -cwd -e test_errors/tiered_5shot_cos_corr_err.txt -o test_results/tiered_5shot_cos_corr_out.psv -N tiered_5shot_cos_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_tiered_5shot_cos.yaml 5
 
+sleep 1h
+
 qsub -m abes -l gpus=1 -cwd -e test_errors/tiered_5shot_sqr_nocorr_err.txt -o test_results/tiered_5shot_sqr_nocorr_out.psv -N tiered_5shot_sqr_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_tiered_5shot_sqr.yaml 5
 qsub -m abes -l gpus=1 -cwd -e test_errors/tiered_5shot_sqr_corr_err.txt -o test_results/tiered_5shot_sqr_corr_out.psv -N tiered_5shot_sqr_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_tiered_5shot_sqr.yaml 5
 
+sleep 1h
+
 qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/tiered_20shot_cos_nocorr_err.txt -o test_results/tiered_20shot_cos_nocorr_out.psv -N tiered_20shot_cos_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_tiered_20shot_cos.yaml 20
 qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/tiered_20shot_cos_corr_err.txt -o test_results/tiered_20shot_cos_corr_out.psv -N tiered_20shot_cos_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_tiered_20shot_cos.yaml 20
+
+sleep 1h
 
 qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/tiered_20shot_sqr_nocorr_err.txt -o test_results/tiered_20shot_sqr_nocorr_out.psv -N tiered_20shot_sqr_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_tiered_20shot_sqr.yaml 20
 qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/tiered_20shot_sqr_corr_err.txt -o test_results/tiered_20shot_sqr_corr_out.psv -N tiered_20shot_sqr_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_tiered_20shot_sqr.yaml 20
