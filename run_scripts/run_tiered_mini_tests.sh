a72e5c392d7a443eeeb16953479b0fb59072fc3c
Nicholas Petrocelli <npetroce@cs.brown.edu>
2022-12-02 20:17:43 -0500

    WIP test running script

--- run_scripts/run_tiered_mini_tests.sh
+++ run_scripts/run_tiered_mini_tests.sh
@@ -23,13 +23,13 @@ qsub -m abes -l gpus=1 -cwd -e test_errors/mini_5shot_sqr_corr_err.txt -o test_r
 
 sleep 1h
 
-qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/mini_20shot_cos_nocorr_err.txt -o test_results/mini_20shot_cos_nocorr_out.psv -N mini_20shot_cos_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_20shot_cos.yaml 20
-qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/mini_20shot_cos_corr_err.txt -o test_results/mini_20shot_cos_corr_out.psv -N mini_20shot_cos_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_20shot_cos.yaml 20
+qsub -m abes -l gpus=1 -cwd -e test_errors/mini_20shot_cos_nocorr_err.txt -o test_results/mini_20shot_cos_nocorr_out.psv -N mini_20shot_cos_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_20shot_cos.yaml 20
+qsub -m abes -l gpus=1 -cwd -e test_errors/mini_20shot_cos_corr_err.txt -o test_results/mini_20shot_cos_corr_out.psv -N mini_20shot_cos_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_20shot_cos.yaml 20
 
 sleep 1h
 
-qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/mini_20shot_sqr_nocorr_err.txt -o test_results/mini_20shot_sqr_nocorr_out.psv -N mini_20shot_sqr_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_20shot_sqr.yaml 20
-qsub -m abes -l gpus=1 -l gmem=24 -cwd -e test_errors/mini_20shot_sqr_corr_err.txt -o test_results/mini_20shot_sqr_corr_out.psv -N mini_20shot_sqr_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_20shot_sqr.yaml 20
+qsub -m abes -l gpus=1 -cwd -e test_errors/mini_20shot_sqr_nocorr_err.txt -o test_results/mini_20shot_sqr_nocorr_out.psv -N mini_20shot_sqr_nocorr run_scripts/run_test_1gpu.sh configs/test/test_few_shot_mini_20shot_sqr.yaml 20
+qsub -m abes -l gpus=1 -cwd -e test_errors/mini_20shot_sqr_corr_err.txt -o test_results/mini_20shot_sqr_corr_out.psv -N mini_20shot_sqr_corr run_scripts/run_test_1gpu_corrupted.sh configs/test/test_few_shot_mini_20shot_sqr.yaml 20
 
 sleep 1h
 
