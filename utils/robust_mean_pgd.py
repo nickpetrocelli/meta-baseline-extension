987b8287b4cc186e7980f48c2aead8e06d9c9c85
Nick <nicholas_petrocelli@brown.edu>
2022-12-11 16:37:09 -0500

    fix?

--- utils/robust_mean_pgd.py
+++ utils/robust_mean_pgd.py
@@ -46,16 +46,16 @@ def robust_mean_pgd(X, eps):
         print(np.shape(w.T))
         print(np.shape(w.T @ Xu))
 
-        nabla_f_w = Xu * Xu - 2 * (w.T @ Xu) @ Xu;
+        nabla_f_w = Xu * Xu - 2 * (w.T @ Xu) * Xu;
         old_w = w;
-        w = w - stepSz @ nabla_f_w / np.linalg.norm(nabla_f_w, ord=2);
+        w = w - stepSz * nabla_f_w / np.linalg.norm(nabla_f_w, ord=2);
         # Projecting w onto the feasible region
         w = project_onto_capped_simplex_simple(w, 1 / (N - epsN))
         
         # Use adaptive step size.
         #   If objective function decreases, take larger steps.
         #   If objective function increases, take smaller steps.
-        Sigma_w_fun = lambda v: X.T @ (w * (X @ v)) - Xw @ Xw.T @ v
+        Sigma_w_fun = lambda v: X.T @ (w * (X @ np.reshape(v, (d, 1)))) - Xw @ Xw.T @ np.reshape(v, (d, 1))
         Sigma_w_op = sla.LinearOperator((d, d), matvec=Sigma_w_fun)
         new_lambda1, _ = sla.eigs(Sigma_w_op, 1)
         #[~, new_lambda1] = eigs(Sigma_w_fun, d, 1);
