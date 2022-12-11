42a63871f3ae70873595e92ddaa204fd8ace901d
Nick <nicholas_petrocelli@brown.edu>
2022-12-11 01:58:59 -0500

    reshaping because matlab numpy discrepancy

--- utils/robust_mean_pgd.py
+++ utils/robust_mean_pgd.py
@@ -40,8 +40,9 @@ def robust_mean_pgd(X, eps):
         # Compute the gradient of spectral norm (assuming unique top eigenvalue)
         # nabla_f_w = (X * u) .* (X * u) - (w' * X * u) * X * u;
         Xu = X @ u;
-        print(np.shape(Xu))
-        print(np.shape(w.T))
+        # I HATE NUMPY I HATE NUMPY
+        Xu = np.reshape(Xu, (1, np.shape(Xu)[0]))
+
         nabla_f_w = Xu * Xu - 2 @ (w.T @ Xu) @ Xu;
         old_w = w;
         w = w - stepSz @ nabla_f_w / np.linalg.norm(nabla_f_w, ord=2);
