1c52c1de2dc1e9dc4ac3da82782e91768fbe92eb
Nick <nicholas_petrocelli@brown.edu>
2022-12-12 23:44:36 -0500

    spurious sleep

--- run_scripts/run_all_pgd_tests.sh
+++ run_scripts/run_all_pgd_tests.sh
@@ -19,7 +19,7 @@
 # sleep 30m
 #qsub -m abes -l gpus=1 -cwd -e test_errors/mini_5shot_pgd_corr_err.txt -o test_results/mini_5shot_pgd_corr_out.csv -N mini_5shot_pgd_corr run_scripts/run_test_1gpu_pgd_corrupted.sh configs/test/test_few_shot_mini_5shot_sqr.yaml 5
 
-sleep 30m
+#sleep 30m
 
 qsub -m abes -l gpus=1 -l gmem=11 -cwd -e test_errors/mini_20shot_pgd_nocorr_err.txt -o test_results/mini_20shot_pgd_nocorr_out.csv -N mini_20shot_pgd_nocorr run_scripts/run_test_1gpu_pgd.sh configs/test/test_few_shot_mini_20shot_sqr.yaml 20
 sleep 3h
